d839c9d1126e262d88344d1b96488086e08c9f0f
aintnewtathis-yy <aintnewtathis@mail.ru>
2024-05-28 19:49:16 +0300

    gogogo

--- src/db/mongo.js
+++ src/db/mongo.js
@@ -2,10 +2,6 @@ import { MongoClient } from "mongodb";
 
 const MONGO_URL = 'mongodb+srv://ilya:AZ2245688q@squaree.1t6swlt.mongodb.net/';
 
-if (!MONGO_URL) {
-    throw new Error("MONGO_URL is not defined in the environment variables.");
-}
-
 const client = new MongoClient(MONGO_URL);
 
 export function startMongo(){
