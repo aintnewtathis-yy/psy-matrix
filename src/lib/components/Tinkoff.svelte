fdab06a0ae8d619e3d023f35d9d23325753dff36
aintnewtathis-yy <aintnewtathis@mail.ru>
2024-06-02 17:53:00 +0300

    demo tink

--- src/lib/components/Tinkoff.svelte
+++ src/lib/components/Tinkoff.svelte
@@ -1,15 +1,57 @@
+<script>
+	const TPF = document.getElementById('payform-tinkoff');
 
+	TPF.addEventListener('submit', function (e) {
+		e.preventDefault();
+		const { description, amount, email, phone, receipt } = TPF;
+
+		if (receipt) {
+			if (!email.value && !phone.value) return alert('Поле E-mail или Phone не должно быть пустым');
+
+			TPF.receipt.value = JSON.stringify({
+				EmailCompany: 'mail@mail.com',
+				Taxation: 'patent',
+				FfdVersion: '1.2',
+				Items: [
+					{
+						Name: description.value || 'Оплата',
+						Price: amount.value + '00',
+						Quantity: 1.0,
+						Amount: amount.value + '00',
+						PaymentMethod: 'full_prepayment',
+						PaymentObject: 'service',
+						Tax: 'none',
+						MeasurementUnit: 'pc'
+					}
+				]
+			});
+		}
+		pay(TPF);
+	});
+</script>
 
 <form class="payform-tinkoff" name="payform-tinkoff" onsubmit="pay(this); return false;">
-    <input class="payform-tinkoff-row" type="hidden" name="terminalkey" value="1717138578425DEMO">
-    <input class="payform-tinkoff-row" type="hidden" name="frame" value="false">
-    <input class="payform-tinkoff-row" type="hidden" name="language" value="ru">
-    <input class="payform-tinkoff-row" type="hidden" name="receipt" value="">
-    <input class="payform-tinkoff-row" type="hidden" placeholder="Сумма заказа" name="amount" value="700" required>
-    <input class="payform-tinkoff-row" type="hidden" placeholder="Номер заказа" name="order">
-    <input class="payform-tinkoff-row" type="hidden" placeholder="Описание заказа" name="description">
-    <input class="payform-tinkoff-row" type="hidden" placeholder="ФИО плательщика" name="name">
-    <input class="payform-tinkoff-row" type="hidden" placeholder="E-mail" name="email">
-    <input class="payform-tinkoff-row" type="hidden" placeholder="Контактный телефон" name="phone">
-    <input class="payform-tinkoff-row payform-tinkoff-btn btn-main" type="submit" value="Оплатить">
+	<input class="payform-tinkoff-row" type="hidden" name="terminalkey" value="1717138578425DEMO" />
+	<input class="payform-tinkoff-row" type="hidden" name="frame" value="false" />
+	<input class="payform-tinkoff-row" type="hidden" name="language" value="ru" />
+	<input class="payform-tinkoff-row" type="hidden" name="receipt" value="" />
+	<input
+		class="payform-tinkoff-row"
+		type="hidden"
+		placeholder="Сумма заказа"
+		name="amount"
+		value="700"
+		required
+	/>
+	<input class="payform-tinkoff-row" type="hidden" placeholder="Номер заказа" name="order" />
+	<input
+		class="payform-tinkoff-row"
+		type="hidden"
+		placeholder="Описание заказа"
+		name="description"
+	/>
+	<input class="payform-tinkoff-row" type="hidden" placeholder="ФИО плательщика" name="name" />
+	<input class="payform-tinkoff-row" type="hidden" placeholder="E-mail" name="email" />
+	<input class="payform-tinkoff-row" type="hidden" placeholder="Контактный телефон" name="phone" />
+	<input class="payform-tinkoff-row payform-tinkoff-btn btn-main" type="submit" value="Оплатить" />
 </form>
