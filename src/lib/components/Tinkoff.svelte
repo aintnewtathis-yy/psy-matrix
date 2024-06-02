df7640b56b8f4aad1970ea1e4559db25ebdb217d
aintnewtathis-yy <aintnewtathis@mail.ru>
2024-06-02 17:56:16 +0300

    123

--- src/lib/components/Tinkoff.svelte
+++ src/lib/components/Tinkoff.svelte
@@ -1,34 +1,3 @@
-<script>
-	const TPF = document.getElementById('payform-tinkoff');
-
-	TPF.addEventListener('submit', function (e) {
-		e.preventDefault();
-		const { description, amount, email, phone, receipt } = TPF;
-
-		if (receipt) {
-			if (!email.value && !phone.value) return alert('Поле E-mail или Phone не должно быть пустым');
-
-			TPF.receipt.value = JSON.stringify({
-				EmailCompany: 'mail@mail.com',
-				Taxation: 'patent',
-				FfdVersion: '1.2',
-				Items: [
-					{
-						Name: description.value || 'Оплата',
-						Price: amount.value + '00',
-						Quantity: 1.0,
-						Amount: amount.value + '00',
-						PaymentMethod: 'full_prepayment',
-						PaymentObject: 'service',
-						Tax: 'none',
-						MeasurementUnit: 'pc'
-					}
-				]
-			});
-		}
-		pay(TPF);
-	});
-</script>
 
 <form class="payform-tinkoff" name="payform-tinkoff" onsubmit="pay(this); return false;">
 	<input class="payform-tinkoff-row" type="hidden" name="terminalkey" value="1717138578425DEMO" />
