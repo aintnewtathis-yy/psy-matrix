dad60769b80d557a833ee16391c7f53c5db51107
aintnewtathis-yy <aintnewtathis@mail.ru>
2024-06-12 04:33:35 +0300

    123

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -3,7 +3,7 @@ import PocketBase from 'pocketbase';
 export const handle = async ({ event, resolve }) => {
 	event.locals.pb = new PocketBase('http://pb.marina-mayskaya.ru/');
 	try {
-		const authData = await event.locals.pb.admins.authWithPassword('1loso@mail.ru', 'AZ2245688q');
+		const authData = await event.locals.pb.admins.authWithPassword('1loso@mail.ru', 'AZ2245688q!@#');
 	} catch (e) {
 		console.error('Error during PocketBase operations:', e);
 	}
