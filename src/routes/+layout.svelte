b7606b9395e5700e2f0a380d40f620bcd251748a
aintnewtathis-yy <aintnewtathis@mail.ru>
2024-06-16 21:57:42 +0300

    ya metric

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -31,7 +31,10 @@
 				</div>
 				<div>
 					<a href="https://t.me/mudrostichisel">телеграм</a>
-					<a href="https://www.instagram.com/ma_rina.mayskaya?igsh=MWNiZjN1Z2VzbWRmYw%3D%3D&utm_source=qr">инстаграм</a>
+					<a
+						href="https://www.instagram.com/ma_rina.mayskaya?igsh=MWNiZjN1Z2VzbWRmYw%3D%3D&utm_source=qr"
+						>инстаграм</a
+					>
 					<a href="https://youtube.com/@marina_mayskaya_?si=-BpbLD4WJfRuqc5y">ютуб</a>
 				</div>
 			</div>
@@ -43,6 +46,34 @@
 		</div>
 	</div>
 </footer>
+
+<svelte:head>
+	<!-- Yandex.Metrika counter -->
+	<script type="text/javascript">
+	(function(m,e,t,r,i,k,a){m[i]=m[i]function(){(m[i].a=m[i].a[]).push(arguments)};
+	m[i].l=1*new Date();
+	for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+	k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+	(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+ 
+	ym(97586428, "init", {
+		 clickmap:true,
+		 trackLinks:true,
+		 accurateTrackBounce:true,
+		 webvisor:true
+	});
+	</script>
+	<noscript
+		><div>
+			<img
+				src="https://mc.yandex.ru/watch/97586428"
+				style="position:absolute; left:-9999px;"
+				alt=""
+			/>
+		</div></noscript
+	>
+	<!-- /Yandex.Metrika counter -->
+</svelte:head>
 
 <style lang="scss">
 	header {
